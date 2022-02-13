8789682148367ddcac590d03f4a72ee04b97d971
Nasekin <nasekinwhiteorblack@gmail.com>
2022-02-13 17:12:04 +0300

    add new solves

--- 8 task.py
+++ 8 task.py
@@ -37,3 +37,44 @@ for comb in permutations(s):
 print(len(res))
 
 1,2,4,6,8,10,12,17,14,
+
+# Светлана составляет коды из букв слова ПАРАБОЛА. Код должен состоять из 8 букв, и каждая буква в нём должна встречаться столько же раз, сколько в заданном слове. Кроме того, в коде не должны стоять рядом две гласные и две согласные буквы.
+
+# Сколько кодов может составить Светлана?
+
+s = "ПРБЛ"
+g = "АО"
+count = 0
+for s1 in s:
+    for s2 in g:
+        for s3 in s:
+            for s4 in g:
+                for s5 in s:
+                    for s6 in g:
+                        for s7 in s:
+                            for s8 in g:
+                                word = s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8
+                                if word.count('П') == 1:
+                                    if word.count('А') == 2:
+                                        if word.count('Р') == 1:
+                                            if word.count('Б') == 1:
+                                                if word.count('О') == 2:
+                                                    if word.count('Л') == 1:
+                                                        count += 1
+for s1 in g:
+    for s2 in s:
+        for s3 in g:
+            for s4 in s:
+                for s5 in g:
+                    for s6 in s:
+                        for s7 in g:
+                            for s8 in s:
+                                word = s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8
+                                if word.count('П') == 1:
+                                    if word.count('А') == 2:
+                                        if word.count('Р') == 1:
+                                            if word.count('Б') == 1:
+                                                if word.count('О') == 2:
+                                                    if word.count('Л') == 1:
+                                                        count += 1
+print(count)
